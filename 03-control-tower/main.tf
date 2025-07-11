01a20c75ab6651d52fc47c772635689ae4b1f994
onoureldin14 <onoureldin@gmail.com>
2025-07-11 10:31:12 +0100

    fix Makefile and bug with Control tower dependency

--- 03-control-tower/main.tf
+++ 03-control-tower/main.tf
@@ -8,11 +8,23 @@ module "iam_roles_ct" {
   source = "./modules/iam"
 }
 
+resource "null_resource" "wait_for_iam_roles" {
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+
+  depends_on = [module.iam_roles_ct]
+}
+
 module "control_tower_landing_zone" {
   source              = "./modules/control-tower-landing-zone"
   governed_regions    = var.governed_regions
   security_account_id = module.organization.account_ids["security"]
   logging_account_id  = module.organization.account_ids["logging"]
   security_org_name   = var.security_ou_name
-  depends_on          = [module.organization, module.iam_roles_ct]
+  depends_on = [
+    module.organization,
+    module.iam_roles_ct,
+    null_resource.wait_for_iam_roles
+  ]
 }
