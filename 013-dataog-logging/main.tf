8531c79a98d1e58ef9957ebf7d41d173ab1ce12d
onoureldin14 <onoureldin@gmail.com>
2025-07-13 10:28:38 +0100

    dd integration

--- 013-dataog-logging/main.tf
+++ 013-dataog-logging/main.tf
@@ -3,7 +3,6 @@ module "datadog_logging" {
   providers = {
     aws = aws.logging
   }
-  datadog_api_key                    = var.datadog_api_key
-  mgt_account_cloudtrail_bucket_name = module.get_cloudtrail_bucket.result[0]
-  mgt_account_cloudtrail_kms_key_arn = data.aws_kms_key.by_alias.arn
+  datadog_api_key        = var.datadog_api_key
+  cloudtrail_kms_key_arn = data.aws_kms_key.by_alias.arn
 }
--- 013-dataog-logging/modules/datadog-logging/variables.tf
+++ 013-dataog-logging/modules/datadog-logging/variables.tf
@@ -29,12 +29,13 @@ variable "cloudwatch_log_group_name" {
 }
 
 
-variable "mgt_account_cloudtrail_bucket_name" {
+variable "cloudtrail_bucket_name" {
   description = "Name of the S3 bucket containing CloudTrail logs to be forwarded."
   type        = string
-}
+  default     = "aws-controltower-logs"
 
-variable "mgt_account_cloudtrail_kms_key_arn" {
+}
+variable "cloudtrail_kms_key_arn" {
   description = "ARN of the KMS key used to encrypt CloudTrail logs. Pass the output from the logging module."
   type        = string
 }
